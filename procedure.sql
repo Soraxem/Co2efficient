52cb67954e90c5e88fdd690e3b9feb92d4924978
cryptoBLD <yann@gaspoz-fleiner.com>
2021-12-27 19:02:26 +0100

    created sql configuration

---
+++ procedure.sql
@@ -0,0 +1,20 @@
+CREATE PROCEDURE 'AddOrUpdateObject'(
+IN _PlantID INT,
+IN _Name varchar(45),
+IN _Value INT
+)
+Begin
+	Declare PlantId INT; Declare Status varchar(20);
+		set PlantId = (select Id from Object where Id = _PlantID);
+		IF PlantId != _PlantID THEN
+		Insert into Object(Id,Name,Value)
+		values (_PlantID,_Name,_Value);
+		Set Status = "Insertion Completed";
+		else
+		update Objects
+		set
+		Name = _Name,
+		Value = _Value,
+		set Status = "Updating Done";
+		END IF;
+		END
---
+++ table.sql
@@ -0,0 +1,6 @@
+create table Objects(
+	Id INT NOT NULL,
+	Name VARCHAR(100) NOT NULL,
+	Value INT NOT NULL,
+	PRIMARY KEY (Id)
+);
